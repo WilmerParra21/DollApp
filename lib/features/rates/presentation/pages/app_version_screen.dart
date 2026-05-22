98a7832def409df29dc556912fea5d942ef839fc
Wilmer Parra <wilmerparra2118@gmail.com>
2026-05-22 00:40:32 -0400

    Corrección de nombre CNY en versión de App

--- lib/features/rates/presentation/pages/app_version_screen.dart
+++ lib/features/rates/presentation/pages/app_version_screen.dart
@@ -169,7 +169,7 @@ class AppVersionScreen extends StatelessWidget {
                         const SizedBox(height: 12),
                         const _FeatureTile(
                           icon: Icons.trending_up_rounded,
-                          text: 'Consulta de tasas BCV, EUR, CYN y COP',
+                          text: 'Consulta de tasas BCV, EUR, CNY y COP',
                         ),
                       
                         const _FeatureTile(
